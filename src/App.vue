ef9b3a8a3b80c5675c9d6f4d0d765e384b79f8ae
kritish-dhaubanjar <kritish.dhaubanjar@gmail.com>
2022-03-03 00:56:37 +0545

    :sparkles: Handle paste: - 192.168.0.1 - 192.168.0.1/ - 192.168.0.1/24

--- src/App.vue
+++ src/App.vue
@@ -46,23 +46,25 @@
       <div
         class="col-12 input-group justify-content-center position-relative ip-address"
       >
-        <template v-for="i in 4" :key="i">
+        <template v-for="(_, i) in ipv4" :key="i">
           <input
-            :value="ipv4[i - 1]"
+            :value="ipv4[i]"
+            @paste="handler"
             @keypress="validate"
-            @input="(e) => setIPv4Segment(e, i - 1)"
+            @input="(e) => setIPv4Segment(e, i)"
             min="0"
             max="255"
             type="number"
             class="form-control text-center"
           />
-          <h1 v-show="i < 4" class="px-3">.</h1>
+          <h1 v-show="i < 3" class="px-3">.</h1>
         </template>
 
         <h1 class="px-3">/</h1>
         <input
           :value="cidr"
           @input="setCIDR"
+          @paste="handler"
           @keypress="validate"
           min="0"
           max="32"
@@ -164,6 +166,7 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 
 import { ref } from "@vue/reactivity";
+import { nextTick, watch } from "@vue/runtime-core";
 
 // @ts-ignore
 import { computeSubnet } from "compute-subnet";
@@ -172,6 +175,10 @@ import { computeSubnet } from "compute-subnet";
 import Segment from "./components/Segment.vue";
 
 const NUMBERS = /[0-9/]+/;
+const IPV4 =
+  /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\/?$/;
+const IPV4_CIDR =
+  /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\/([0-9]|1[0-9]|2[0-9]|3[0-2])$/;
 
 const cidr = ref(23);
 const ipv4 = ref([192, 168, 100, 128]);
@@ -197,6 +204,44 @@ function setCIDR(e: Event) {
   subnet.value = computeSubnet(ipv4.value, cidr.value);
 }
 
+function handler(e: ClipboardEvent) {
+  const clipboard = e?.clipboardData?.getData("text") ?? "";
+
+  const data: {
+    ipv4: number[];
+    cidr: number;
+  } = {
+    ipv4: [192, 168, 100, 128],
+    cidr: cidr.value,
+  };
+
+  if (!IPV4.test(clipboard) && !IPV4_CIDR.test(clipboard)) {
+    return;
+  }
+
+  if (IPV4.test(clipboard)) {
+    data.ipv4 = clipboard
+      .replace(/\//g, "")
+      .split(".")
+      .map((e) => parseInt(e));
+    data.cidr = cidr.value;
+  }
+
+  if (IPV4_CIDR.test(clipboard)) {
+    const [IP, CIDR] = clipboard.split("/");
+    const value = IP.split(".").map((e) => parseInt(e));
+
+    data.ipv4 = value;
+    data.cidr = parseInt(CIDR);
+  }
+
+  nextTick(() => {
+    ipv4.value = data.ipv4;
+    cidr.value = data.cidr;
+    subnet.value = computeSubnet(data.ipv4, data.cidr);
+  });
+}
+
 function setIPv4Segment(e: Event, index: number) {
   const valid = [...ipv4.value];
   const data = parseInt((e.target as HTMLInputElement).value);
@@ -210,6 +255,10 @@ function setIPv4Segment(e: Event, index: number) {
   ipv4.value = valid;
   subnet.value = computeSubnet(ipv4.value, cidr.value);
 }
+
+watch(ipv4, (value, oldValue) => {
+  console.log({ value, oldValue });
+});
 </script>
 
 <style lang="scss">
